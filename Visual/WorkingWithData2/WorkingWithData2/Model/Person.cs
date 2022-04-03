c8ead82859fe9eb675637ab01b3d7a47a8813970
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-04-03 11:07:04 +0600

    Learning events
    Some changes by modifying class Person

--- Visual/WorkingWithData2/WorkingWithData2/Model/Person.cs
+++ Visual/WorkingWithData2/WorkingWithData2/Model/Person.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WorkingWithData2.Model
 {
-    public class Person
+    public class Person: INotifyPropertyChanged
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; OnPropertyChanged("FirstName"); }
+        }
+
+        string lastName;
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; OnPropertyChanged("LastName"); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
     }
 }
