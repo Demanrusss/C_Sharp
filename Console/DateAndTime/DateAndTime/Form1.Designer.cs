47ac6bf0e5fc39579202789ce0d39c9c00337b6a
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-04-11 21:54:18 +0600

    Date and Time
    Learning Date and Time

---
+++ Console/DateAndTime/DateAndTime/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace DateAndTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yearNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.monthNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.dayNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.hourNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.minuteNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dayOfWeekLabel = new System.Windows.Forms.Label();
+            this.dayOfWeekNumberLabel = new System.Windows.Forms.Label();
+            this.timeOfDayLabel = new System.Windows.Forms.Label();
+            this.dayOfYearLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.yearNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monthNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dayNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // yearNumericUpDown
+            // 
+            this.yearNumericUpDown.Location = new System.Drawing.Point(30, 24);
+            this.yearNumericUpDown.Name = "yearNumericUpDown";
+            this.yearNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.yearNumericUpDown.TabIndex = 0;
+            // 
+            // monthNumericUpDown
+            // 
+            this.monthNumericUpDown.Location = new System.Drawing.Point(30, 53);
+            this.monthNumericUpDown.Name = "monthNumericUpDown";
+            this.monthNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.monthNumericUpDown.TabIndex = 1;
+            // 
+            // dayNumericUpDown
+            // 
+            this.dayNumericUpDown.Location = new System.Drawing.Point(30, 82);
+            this.dayNumericUpDown.Name = "dayNumericUpDown";
+            this.dayNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.dayNumericUpDown.TabIndex = 2;
+            // 
+            // hourNumericUpDown
+            // 
+            this.hourNumericUpDown.Location = new System.Drawing.Point(30, 111);
+            this.hourNumericUpDown.Name = "hourNumericUpDown";
+            this.hourNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.hourNumericUpDown.TabIndex = 3;
+            // 
+            // minuteNumericUpDown
+            // 
+            this.minuteNumericUpDown.Location = new System.Drawing.Point(30, 140);
+            this.minuteNumericUpDown.Name = "minuteNumericUpDown";
+            this.minuteNumericUpDown.Size = new System.Drawing.Size(120, 22);
+            this.minuteNumericUpDown.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Click on me";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dayOfWeekLabel
+            // 
+            this.dayOfWeekLabel.AutoSize = true;
+            this.dayOfWeekLabel.Location = new System.Drawing.Point(157, 58);
+            this.dayOfWeekLabel.Name = "dayOfWeekLabel";
+            this.dayOfWeekLabel.Size = new System.Drawing.Size(44, 16);
+            this.dayOfWeekLabel.TabIndex = 7;
+            this.dayOfWeekLabel.Text = "label2";
+            // 
+            // dayOfWeekNumberLabel
+            // 
+            this.dayOfWeekNumberLabel.AutoSize = true;
+            this.dayOfWeekNumberLabel.Location = new System.Drawing.Point(157, 87);
+            this.dayOfWeekNumberLabel.Name = "dayOfWeekNumberLabel";
+            this.dayOfWeekNumberLabel.Size = new System.Drawing.Size(44, 16);
+            this.dayOfWeekNumberLabel.TabIndex = 8;
+            this.dayOfWeekNumberLabel.Text = "label3";
+            // 
+            // timeOfDayLabel
+            // 
+            this.timeOfDayLabel.AutoSize = true;
+            this.timeOfDayLabel.Location = new System.Drawing.Point(157, 116);
+            this.timeOfDayLabel.Name = "timeOfDayLabel";
+            this.timeOfDayLabel.Size = new System.Drawing.Size(44, 16);
+            this.timeOfDayLabel.TabIndex = 9;
+            this.timeOfDayLabel.Text = "label4";
+            // 
+            // dayOfYearLabel
+            // 
+            this.dayOfYearLabel.AutoSize = true;
+            this.dayOfYearLabel.Location = new System.Drawing.Point(160, 145);
+            this.dayOfYearLabel.Name = "dayOfYearLabel";
+            this.dayOfYearLabel.Size = new System.Drawing.Size(44, 16);
+            this.dayOfYearLabel.TabIndex = 10;
+            this.dayOfYearLabel.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dayOfYearLabel);
+            this.Controls.Add(this.timeOfDayLabel);
+            this.Controls.Add(this.dayOfWeekNumberLabel);
+            this.Controls.Add(this.dayOfWeekLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.minuteNumericUpDown);
+            this.Controls.Add(this.hourNumericUpDown);
+            this.Controls.Add(this.dayNumericUpDown);
+            this.Controls.Add(this.monthNumericUpDown);
+            this.Controls.Add(this.yearNumericUpDown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.yearNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.monthNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dayNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hourNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.minuteNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown yearNumericUpDown;
+        private System.Windows.Forms.NumericUpDown monthNumericUpDown;
+        private System.Windows.Forms.NumericUpDown dayNumericUpDown;
+        private System.Windows.Forms.NumericUpDown hourNumericUpDown;
+        private System.Windows.Forms.NumericUpDown minuteNumericUpDown;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label dayOfWeekLabel;
+        private System.Windows.Forms.Label dayOfWeekNumberLabel;
+        private System.Windows.Forms.Label timeOfDayLabel;
+        private System.Windows.Forms.Label dayOfYearLabel;
+    }
+}
+
---
+++ Console/DateAndTime/DateAndTime/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateAndTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year = (int)yearNumericUpDown.Value;
+            int month = (int)monthNumericUpDown.Value;
+            int day = (int)dayNumericUpDown.Value;
+            int hour = (int)hourNumericUpDown.Value;
+            int minute = (int)minuteNumericUpDown.Value;
+
+            //DateTime dt = new DateTime(year, month, day, hour, minute, 0);
+            DateTime dt = DateTime.Now;
+
+            TimeSpan ts = new TimeSpan(-1, 10, 0);
+
+            dt = dt.Add(ts);
+
+
+            dayOfWeekLabel.Text = dt.DayOfWeek.ToString();
+            dayOfWeekNumberLabel.Text = ((int)dt.DayOfWeek).ToString();
+            timeOfDayLabel.Text = dt.TimeOfDay.ToString();
+            dayOfYearLabel.Text = dt.DayOfYear.ToString();
+        }
+    }
+}
