bde69a3237d55c0b456bc4c01a9a9cc27562cc99
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-04-13 13:21:32 +0600

    StringProject
    Learn more about string and String

---
+++ Console/StringProject/StringProject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strSource, strToSearch;
+
+            Console.WriteLine("Enter one string: ");
+            strSource = Console.ReadLine();
+            Console.WriteLine("Enter second string for searching: ");
+            strToSearch = Console.ReadLine();
+
+            // method Contains()
+            if (strSource.Contains(strToSearch))
+                Console.WriteLine("String1 contains string2");
+
+            // static method Format()
+            String strOfClassString;
+            strOfClassString = String.Format("Hello {0} and let's get the {1} rolling!",
+                "world", "ball");
+            Console.WriteLine(strOfClassString);
+        }
+    }
+}
