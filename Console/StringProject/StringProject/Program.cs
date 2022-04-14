0bcbe2c76d8e537de16cba9dc97bc88883c8b3d0
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-04-14 14:40:36 +0600

    Learning more about String methods

--- Console/StringProject/StringProject/Program.cs
+++ Console/StringProject/StringProject/Program.cs
@@ -26,6 +26,57 @@ namespace StringProject
             strOfClassString = String.Format("Hello {0} and let's get the {1} rolling!",
                 "world", "ball");
             Console.WriteLine(strOfClassString);
+
+            // method IndexOf()
+            int index = strSource.IndexOf(strToSearch);
+            if (index != -1)
+                Console.WriteLine("Found at " + index);
+            
+            // method Insert()
+            string strExample = strSource.Insert(5, "example");
+            Console.WriteLine(strExample);
+
+            //methods PadLeft(int N) and PadRight(int N)
+            strExample = strSource.PadLeft(50);
+            Console.WriteLine(strExample);
+            strExample = strSource.PadRight(50);
+            Console.WriteLine(strExample);
+
+            // method Remove()
+            strExample = strSource.Remove(5, 5);
+            Console.WriteLine("After Remove " + strExample);
+
+            //method Replace()
+            strExample = strSource.Replace(strToSearch, "WORLD");
+            Console.WriteLine("After Replace " + strExample);
+
+            // method ToUpper() and ToLower()
+            strExample = strSource.ToUpper();
+            Console.WriteLine("ToUpper " + strExample);
+            Console.WriteLine("ToLower " + strExample.ToLower());
+
+            // metod Substring()
+            strExample = strSource.Substring(5);
+            Console.WriteLine("Substring after 5th symbol " + strExample);
+
+            // method ToCharArray()
+            char[] chars = strSource.ToCharArray();
+            foreach (char symbol in chars)
+                Console.Write(symbol + "\'");
+
+            Console.WriteLine("\n");
+
+            // check up a string for emptiness
+            Console.WriteLine("Enter some sentence, I'll check it up for emptiness.");
+            strSource = Console.ReadLine();
+
+            // first option of realisation
+            if (strSource == null || strSource == "")
+                Console.WriteLine("Your entered nothing.");
+
+            // second option of realisation by static method IsNullOrEmpty()
+            if (String.IsNullOrEmpty(strSource))
+                Console.WriteLine("Your entered nothing...");
         }
     }
 }
