9e9096798c6ec54443185f842c49c9208f5935ff
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-04-18 10:18:01 +0600

    Understanding Templates

---
+++ Console/TemplateProject/TemplateProject/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TemplateTest<int> testarray = new TemplateTest<int>();
+
+            testarray.Add(10);
+            testarray.Add(1);
+            testarray.Add(3);
+            testarray.Add(14);
+
+            for (int i = 0; i <= testarray.Count() - 1; i++)
+                Console.WriteLine(testarray.Get(i));
+
+            TemplateTest<string> testarray2 = new TemplateTest<string>();
+
+            testarray2.Add("Michail");
+            testarray2.Add("Flenov");
+            testarray2.Add("Dmitriy");
+            testarray2.Add("Yevchenko");
+
+            for (int i = 0; i <= testarray2.Count() - 1; i++)
+                Console.WriteLine(testarray2.Get(i));
+        }
+    }
+}
---
+++ Console/TemplateProject/TemplateProject/TemplateTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateProject
+{
+    // Also we can use: public class TemplateTest<T> where T : XXXXX
+    // XXXXX: Class, struct, new(), Name_of_Class, Name_of_Interface
+    // Example:
+    // public class TemplateTest<T> where T : Person
+
+
+    // Here is universal one
+    public class TemplateTest<T>
+    {
+        T[] array = new T[10];
+        int index = 0;
+
+        public bool Add(T value)
+        {
+            if (index >= 10)
+                return false;
+
+            array[index++] = value;
+            return true;
+        }
+
+        public T Get(int index)
+        {
+            if (index < this.index && index >= 0)
+                return array[index];
+            else
+                return default(T);
+        }
+
+        public int Count()
+        {
+            return index;
+        }
+    }
+}
