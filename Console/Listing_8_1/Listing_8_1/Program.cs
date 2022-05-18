e14efb7821bfe78c58ede709cd6aca9e07d3cac0
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-05-18 16:20:09 +0600

    Learning about arrays

---
+++ Console/Listing_8_1/Listing_8_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Listing_8_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 10, 20, 1, 6, 15};
+
+            // Print out array
+            foreach (int i in test)
+                Console.WriteLine(i);
+
+            // Sorting
+            Console.WriteLine("Sorted array");
+            Array.Sort(test);
+            foreach (int i in test)
+                Console.WriteLine(i);
+
+            // Reverse
+            Console.WriteLine("Reversed array");
+            Array.Reverse(test);
+            foreach (int i in test)
+                Console.WriteLine(i);
+
+            // Deleting two items
+            Console.WriteLine("Length before clear off {0}", test.Length);
+            Array.Clear(test, 2, 2);
+            Console.WriteLine("Length after clear off {0}", test.Length);
+            foreach (int i in test)
+                Console.WriteLine(i);
+        }
+    }
+}
---
+++ Console/Listing_8_2/Listing_8_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing_8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[10][];
+
+            // Memory allocation for each row of the array
+            for (int i = 0; i < jaggedArray.Length; i++)
+                jaggedArray[i] = new int[i];
+
+            // Using the array
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    jaggedArray[i][j] = j;
+                    Console.Write(jaggedArray[i][j]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
