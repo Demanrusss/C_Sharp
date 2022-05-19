fd508a9c030c8ba17a166bc0ab152896d85a1101
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-05-19 16:54:37 +0600

    Working with dynamic array

---
+++ Visual/Listing_8_3/Listing_8_3/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listing_8_3
+{
+    internal class Person
+    {
+        private string name;
+        private string surname;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public Person (string firstName, string lastName)
+        {
+            Name = firstName;
+            Surname = lastName;
+        }
+    }
+}
---
+++ Visual/Listing_8_3/Listing_8_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Listing_8_3
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            ArrayList Children = new ArrayList();
+        }
+
+        private static void AddChild(string firstName, string lastName, ArrayList Children)
+        {
+            Children.Add(new Person(firstName, lastName));
+        }
+
+        private static void DeleteChild(int index, ArrayList Children)
+        {
+            Children.Remove(index);
+        }
+
+        private static Person GetChild(int index, ArrayList Children)
+        {
+            return (Person)Children[index];
+        }
+    }
+}
