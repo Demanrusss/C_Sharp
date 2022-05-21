0cb3e38c01eeda0cd5a48e35a6bf6bd779e453f3
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-05-21 09:15:24 +0600

    Listing 8_4 PersonEnumerator

---
+++ Visual/ArrayListProject/ArrayListProject/PersonClass/Person.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using ArrayListProject;
+
+namespace PersonClass
+{
+    internal class Person
+    {
+        // Devoting variables
+        private string name;
+        private string surname;
+
+        // Devoiting an array
+        ArrayList Children = new ArrayList();
+
+        // Devoting properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        // Devoting constructor
+        public Person(string firstName, string lastName)
+        {
+            Name = firstName;
+            Surname = lastName;
+        }
+
+        // Devoting Methods
+        public void AddChild(string firstName, string lastName)
+        {
+            Children.Add(new Person(firstName, lastName));
+        }
+        public void DeleteChild(int index)
+        {
+            Children.RemoveAt(index);
+        }
+        public Person GetChild(int index)
+        {
+            return (Person)Children[index];
+        }
+        public int GetChildrenNumber()
+        {
+            return Children.Count;
+        }
+        public Person this[int index]
+        {
+            get { return (Person)Children[index]; }
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return new PersonEnumerator(this);
+        }
+    }
+}
---
+++ Visual/ArrayListProject/ArrayListProject/PersonEnumerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+
+namespace ArrayListProject
+{
+    internal class PersonEnumerator : IEnumerator
+    {
+        int currIndex = -1;
+        PersonClass.Person person;
+
+        public PersonEnumerator(PersonClass.Person person)
+        {
+            this.person = person;
+        }
+
+        #region IEnumerator Members
+
+        public object Current
+        {
+            get { return person[currIndex]; }
+        }
+
+        public bool MoveNext()
+        {
+            currIndex++;
+            if (currIndex >= person.GetChildrenNumber())
+                return false;
+            else
+                return true;
+        }
+
+        public void Reset()
+        {
+            currIndex = -1;
+        }
+
+        #endregion
+    }
+}
