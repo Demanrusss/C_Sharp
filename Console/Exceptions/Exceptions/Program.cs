6ac14bf28c72ffff94c27f462945f0c024c292cc
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-05-25 16:57:46 +0600

    Working with exceptions

---
+++ Console/Exceptions/Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter some integer: ");
+
+                string inLine = Console.ReadLine();
+                if (inLine == "q")
+                    break;
+                try
+                {
+                    int i = Convert.ToInt32(inLine);
+                    Console.WriteLine($"You have entered: {i}");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"You have entered the incorrect number {inLine}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Visual/Exceptions/Exceptions/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exceptions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int index = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show($"You have entered {index}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show($"You have entered incorrect number");
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unknown problem: {ex.Message}");
+                return;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index;
+
+            try
+            {
+                index = Convert.ToInt32(textBox1.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show($"You have entered incorrect number");
+                return;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Unknown problem: {ex.Message}");
+                return;
+            }
+
+            if (index < 0)
+                throw new Exception("You have entered a number < 0");
+            if (index > 1000)
+                throw new Exception("You have entered a number > 1000");
+
+            MessageBox.Show($"You have entered {index}");
+        }
+    }
+}
