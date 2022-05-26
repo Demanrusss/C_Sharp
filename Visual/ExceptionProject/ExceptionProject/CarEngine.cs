77fd8805f39f44c191ab9d31d99ff1249382523c
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-05-26 13:31:22 +0600

    Exceptions2

---
+++ Visual/ExceptionProject/ExceptionProject/CarEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionProject
+{
+    public class CarEngine
+    {
+        public string Name { get; set; }
+        public bool Working { get; private set; }
+
+        public CarEngine(string name)
+        {
+            Working = false;
+            Name = name;
+        }
+
+        public void StartEngine()
+        {
+            if (Working)
+                throw new EngineException(this, "The engine is already working");
+
+            Working = true;
+        }
+
+        public void StopEngine()
+        {
+            Working = false;
+        }
+    }
+
+    public class EngineException : ApplicationException
+    {
+        CarEngine engine;
+
+        public EngineException(CarEngine engine, string message) : base(message)
+        {
+            this.engine = engine;
+        }
+        public CarEngine Engine
+        {
+            get { return engine; }
+        }
+    }
+}
---
+++ Visual/ExceptionProject/ExceptionProject/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExceptionProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CarEngine engine;
+
+            try
+            {
+                engine.StartEngine();
+            }
+            catch (EngineException ex)
+            {
+                MessageBox.Show("The engine '" + ex.Engine.Name + "'\nIt generated a mistake: '" 
+                    + ex.Message + "'");
+            }
+        }
+    }
+}
