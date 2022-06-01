2f6d76410915f592a6c8f52fb26e6c28fd0ed29a
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-01 10:55:35 +0600

    Working with memory

---
+++ Console/ArrayProject/ArrayProject/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayProject
+{
+    internal class Program
+    {
+        unsafe static void Main(string[] args)
+        {
+            int[] managedarray = { 10, 20, 5, 2, 54, 9 };
+
+            // Getting some memory in stack
+            int* array = stackalloc int[managedarray.Length];
+
+            Console.WriteLine("Copying values from managedarray to array");
+            for (int i = 0; i < managedarray.Length; i++)
+            {
+                array[i] = managedarray[i];
+                Console.WriteLine("The value: " + array[i]);
+            }
+
+            // Fixed array
+            Console.WriteLine("\nUsing fixed array");
+
+            fixed (int* arrayPtr = &managedarray[0])
+            {
+                for (int i = 0; i < managedarray.Length; i++)
+                    Console.WriteLine("The value of fixed array: " + arrayPtr[i]);
+            }
+
+            // Understanding how much memory is used for variables
+            int intSize = sizeof(int);
+            Console.WriteLine($"\nThe memory used for int is - {intSize} bytes");
+        }
+    }
+}
