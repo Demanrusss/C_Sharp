6a755b218d75c6e690b98156d7165655ed7c3908
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-01 09:42:33 +0600

    Learning LINQ and XML

---
+++ Console/LinqXml/LinqXml/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinqXml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XElement root = XElement.Load(@"D:\Coding\C_Sharp\Console\LinqXml\person.xml");
+
+            // Filter people who under 30
+            Console.WriteLine("Person who is under 30");
+            foreach (var item in root.Elements("person").Where(m =>
+                Int32.Parse(m.Attribute("Age").Value) < 30))
+            {
+                Console.WriteLine(item.Attribute("FirstName").Value + " " +
+                    item.Attribute("Age"));
+            }
+
+            // Filter by city in address
+            Console.WriteLine("Person who lives in Moskow");
+            foreach (var item in root.Elements("person").Where(m =>
+                m.Elements("address").Elements("city").FirstOrDefault().Value == "Moskow"))
+            {
+                Console.WriteLine(item.Attribute("FirstName").Value + " " + 
+                    item.Attribute("LastName").Value);
+            }
+        }
+    }
+}
