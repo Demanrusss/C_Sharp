a471dd1621b1f78c523c20abda0c207a8b905004
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-02 13:29:49 +0600

    Creating file manager

---
+++ Visual/DirFileProject/DirFileProject/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DirFileProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.fmListView = new System.Windows.Forms.ListView();
+            this.iconsImageList = new System.Windows.Forms.ImageList(this.components);
+            this.hideFilesCheckBox = new System.Windows.Forms.CheckBox();
+            this.parentDirectoryButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fmListView
+            // 
+            this.fmListView.HideSelection = false;
+            this.fmListView.LargeImageList = this.iconsImageList;
+            this.fmListView.Location = new System.Drawing.Point(12, 64);
+            this.fmListView.Name = "fmListView";
+            this.fmListView.Size = new System.Drawing.Size(657, 260);
+            this.fmListView.SmallImageList = this.iconsImageList;
+            this.fmListView.TabIndex = 0;
+            this.fmListView.UseCompatibleStateImageBehavior = false;
+            this.fmListView.View = System.Windows.Forms.View.List;
+            this.fmListView.ItemActivate += new System.EventHandler(this.fmListView_ItemActivate);
+            // 
+            // iconsImageList
+            // 
+            this.iconsImageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("iconsImageList.ImageStream")));
+            this.iconsImageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.iconsImageList.Images.SetKeyName(0, "yast-file.ico");
+            this.iconsImageList.Images.SetKeyName(1, "folder-yellow.ico");
+            // 
+            // hideFilesCheckBox
+            // 
+            this.hideFilesCheckBox.AutoSize = true;
+            this.hideFilesCheckBox.Location = new System.Drawing.Point(565, 41);
+            this.hideFilesCheckBox.Name = "hideFilesCheckBox";
+            this.hideFilesCheckBox.Size = new System.Drawing.Size(104, 17);
+            this.hideFilesCheckBox.TabIndex = 1;
+            this.hideFilesCheckBox.Text = "Hide system files";
+            this.hideFilesCheckBox.UseVisualStyleBackColor = true;
+            this.hideFilesCheckBox.CheckedChanged += new System.EventHandler(this.hideFilesCheckBox_CheckedChanged);
+            // 
+            // parentDirectoryButton
+            // 
+            this.parentDirectoryButton.Location = new System.Drawing.Point(12, 37);
+            this.parentDirectoryButton.Name = "parentDirectoryButton";
+            this.parentDirectoryButton.Size = new System.Drawing.Size(23, 23);
+            this.parentDirectoryButton.TabIndex = 2;
+            this.parentDirectoryButton.Text = "..";
+            this.parentDirectoryButton.UseVisualStyleBackColor = true;
+            this.parentDirectoryButton.Click += new System.EventHandler(this.parentDirectoryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 336);
+            this.Controls.Add(this.parentDirectoryButton);
+            this.Controls.Add(this.hideFilesCheckBox);
+            this.Controls.Add(this.fmListView);
+            this.Name = "Form1";
+            this.Text = "Directories/Files";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView fmListView;
+        private System.Windows.Forms.ImageList iconsImageList;
+        private System.Windows.Forms.CheckBox hideFilesCheckBox;
+        private System.Windows.Forms.Button parentDirectoryButton;
+    }
+}
+
---
+++ Visual/DirFileProject/DirFileProject/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DirFileProject
+{
+    public partial class Form1 : Form
+    {
+        string FPath = "C:\\";
+        public Form1()
+        {
+            InitializeComponent();
+            GetFiles();
+        }
+
+        void GetFiles()
+        {
+            // Start updating
+            fmListView.BeginUpdate();
+            fmListView.Items.Clear();
+
+            // Get list of directories
+            string[] dirs = Directory.GetDirectories(FPath);
+            foreach (string dir in dirs)
+            {
+                string dirName = Path.GetFileName(dir);
+                fmListView.Items.Add(dirName, 1);
+            }
+
+            // Get list of files
+            string[] files = Directory.GetFiles(FPath);
+            foreach (string file in files)
+            {
+                string fileName = Path.GetFileName(file);
+                if ((File.GetAttributes(file) & FileAttributes.Hidden) == FileAttributes.Hidden)
+                    if (hideFilesCheckBox.Checked)
+                        continue;
+
+                fmListView.Items.Add(fileName, 0);
+            }
+
+            // End updating
+            fmListView.EndUpdate();
+        }
+
+        private void hideFilesCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            GetFiles();
+        }
+
+        private void fmListView_ItemActivate(object sender, EventArgs e)
+        {
+            if (fmListView.SelectedItems.Count == 0)
+                return;
+
+            ListViewItem item = fmListView.SelectedItems[0];
+
+            if (item.ImageIndex == 1)
+            {
+                FPath = FPath + item.Text + "\\";
+                GetFiles();
+            }
+        }
+
+        private void parentDirectoryButton_Click(object sender, EventArgs e)
+        {
+            if (Directory.GetParent(FPath) != null)
+            {
+                FPath = Directory.GetParent(FPath).FullName.Replace(Directory.GetParent(FPath).Name, "");
+                GetFiles();
+            }
+        }
+    }
+}
