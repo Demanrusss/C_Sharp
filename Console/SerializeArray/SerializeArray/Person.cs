5c65e6a0a1efc2fc9555fb039ef54440057e215f
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-07 16:43:17 +0600

    Serialization with array

---
+++ Console/SerializeArray/SerializeArray/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeArray
+{
+    [Serializable]
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        // Constructor
+        public Person(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+    }
+}
---
+++ Console/SerializeArray/SerializeArray/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializeArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saving data
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person("Ivan", "Ivanov"));
+            persons.Add(new Person("Sergei", "Petrov"));
+
+            // Create a file
+            FileStream fsout = new FileStream("people.dat", FileMode.Create, 
+                FileAccess.Write);
+
+            // serialization of data
+            XmlSerializer serializerout = new XmlSerializer(typeof(List<Person>), 
+                new Type[] {typeof(Person)});
+            serializerout.Serialize(fsout, persons);
+            fsout.Close();
+
+            // Load data
+            List<Person> persons1 = new List<Person>();
+            FileStream fsin = new FileStream("peoples.dat", FileMode.Open, 
+                FileAccess.Read);
+
+            // Deserialization data
+            XmlSerializer serializerin = new XmlSerializer(typeof(List<Person>),
+                new Type[] { typeof(Person) });
+            persons1 = (List<Person>)serializerin.Deserialize(fsin);
+            fsin.Close();
+
+            // Check up
+            foreach (Person person in persons1)
+                Console.WriteLine(person.Name);
+            Console.ReadLine();
+        }
+    }
+}
