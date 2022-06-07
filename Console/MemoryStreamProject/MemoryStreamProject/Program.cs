daafb9ba01beb1b5b6c545307b1e7f9e88373dbc
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-07 15:29:14 +0600

    Learning how to use Memory stream

---
+++ Console/MemoryStreamProject/MemoryStreamProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace MemoryStreamProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string STRING_EXAMPLE = "This string we are going to put into memory";
+
+            // Transform string to array of symbols
+            UnicodeEncoding unicode = new UnicodeEncoding();
+            byte[] bytes = unicode.GetBytes(STRING_EXAMPLE);
+            int string_size = unicode.GetByteCount(STRING_EXAMPLE);
+
+            // Create memory stream and write data into it
+            MemoryStream mstream = new MemoryStream(string_size);
+            mstream.Write(bytes, 0, string_size);
+
+            // Move to the very beginning of the stream
+            mstream.Seek(0, SeekOrigin.Begin);
+
+            // Create buffer
+            byte[] buffer = new byte[string_size];
+
+            // Read the stream of data
+            mstream.Read(buffer, 0, string_size);
+            Console.WriteLine(unicode.GetString(buffer));
+        }
+    }
+}
