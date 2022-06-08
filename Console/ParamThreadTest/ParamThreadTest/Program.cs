8150a1980220a998477bced227309f0d79f92829
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-08 10:04:47 +0600

    Learning how to use Threads with parameters

---
+++ Console/ParamThreadTest/ParamThreadTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ParamThreadTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadProc));
+            t.IsBackground = true;
+            t.Start(5);
+            string s;
+
+            do
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(s);
+            } while (s != "q");
+        }
+
+        public static void ThreadProc(object number)
+        {
+            int loop_number = (int)number;
+            for (int i = 0; i < loop_number; i++)
+            {
+                Console.WriteLine("This is a child stream");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
