df08b73dab69de6c1a6a48758b3ba5e27608071e
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-08 09:55:49 +0600

    Learning how to use Threads

---
+++ Console/ThreadTest/ThreadTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadProc));
+            // Just not to wait the end of the child stream
+            //t.IsBackground = true;
+            t.Start();
+            string s;
+
+            do
+            {
+                s = Console.ReadLine();
+                Console.WriteLine(s);
+            } while (s != "q");
+        }
+
+        public static void ThreadProc()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("This is a stream");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
