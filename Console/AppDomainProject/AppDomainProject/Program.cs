f6339b38527a29ead94c0aa92795fd9322177fae
Dmitriy Yevchenko <102857752+Demanrusss@users.noreply.github.com>
2022-06-08 14:20:59 +0600

    Learning how to use AppDomain

---
+++ Console/AppDomainProject/AppDomainProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDomainProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("So now we are about to launch another build");
+
+            AppDomain appD = AppDomain.CreateDomain("My Domain");
+            appD.ExecuteAssembly(@"D:\Coding\C_Sharp\Console\DomainTest\DomainTest\bin\Debug\DomainTest.exe");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Console/DomainTest/DomainTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is external build");
+            Console.WriteLine(AppDomain.CurrentDomain.FriendlyName);
+            Console.ReadLine();
+            Console.WriteLine("Shutting down the task");
+        }
+    }
+}
