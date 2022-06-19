19ebe569696b16e668582d1ce8dbf5802f3a05d1
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-19 17:44:02 +0600

    Get started with SQL DB

---
+++ Visual/DBConnectionProject/DBConnectionProject/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DBConnectionProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = @"Provider=SQLOLEDB.1;
+                Integrated Security=SSPI;Persist Security Info=False;
+                Initial Catalog=TestDatabase;Data Source=localhost\SQLEXPRESS";
+            
+            // one way of working with DB
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("There is a mistake while connecting to DB");
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                    connection.Close();
+            }
+
+            // Better way of working with DB
+            using (connection)
+            {
+                connection.Open();
+
+                // First method to get a responce
+                OleDbCommand command = connection.CreateCommand();
+                command.CommandText = "SELECT COUNT(*) FROM Peoples";
+                int number = (int)command.ExecuteScalar();
+                MessageBox.Show(number.ToString());
+
+                // Second method to get a responce
+                OleDbCommand command2 = 
+                    new OleDbCommand("SELECT COUNT(*) FROM Peoples", connection);
+                MessageBox.Show(command2.ExecuteScalar().ToString());
+            }
+        }
+    }
+}
