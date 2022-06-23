3bdc1839d226c9a0c4a4a31f750955b4987d8850
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-23 09:46:05 +0600

    Added some columns

--- Visual/DataTableProject/DataTableProject/Form1.cs
+++ Visual/DataTableProject/DataTableProject/Form1.cs
@@ -19,15 +19,18 @@ namespace DataTableProject
             table = new DataTable();
 
             // Table columns
-            DataColumn c = table.Columns.Add("idKey", typeof(string));
+            DataColumn c = table.Columns.Add("idKey", typeof(String));
             c.AutoIncrement = true;
             c.AutoIncrementSeed = 10;
             c.AutoIncrementStep = 5;
             c.ReadOnly = true;
 
-            table.Columns.Add("Goods", typeof(string));
+            table.Columns.Add("Goods", typeof(String));
             table.Columns.Add("Quantity", typeof(Int32));
             table.Columns.Add("Price", typeof(Int32));
+            table.Columns.Add("Total", typeof(String), "Quantity * Price");
+            table.Columns.Add("More than 100", typeof(Boolean), "Quantity * Price > 100");
+            table.Columns.Add("Sum", typeof(String), "Sum(Quantity)");
 
             // Binding and fill in dataGridView
             bindingSource.DataSource = table;
