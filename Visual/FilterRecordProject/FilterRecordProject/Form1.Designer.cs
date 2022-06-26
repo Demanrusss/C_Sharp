57ebedc20e09cb0326d4eb957f3a294f66a47baf
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-26 16:15:59 +0600

    Learning how to use filter

---
+++ Visual/FilterRecordProject/FilterRecordProject/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FilterRecordProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.findPersonTextBox = new System.Windows.Forms.TextBox();
+            this.findPersonButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // findPersonTextBox
+            // 
+            this.findPersonTextBox.Location = new System.Drawing.Point(13, 13);
+            this.findPersonTextBox.Name = "findPersonTextBox";
+            this.findPersonTextBox.Size = new System.Drawing.Size(100, 22);
+            this.findPersonTextBox.TabIndex = 0;
+            // 
+            // findPersonButton
+            // 
+            this.findPersonButton.Location = new System.Drawing.Point(119, 13);
+            this.findPersonButton.Name = "findPersonButton";
+            this.findPersonButton.Size = new System.Drawing.Size(110, 23);
+            this.findPersonButton.TabIndex = 1;
+            this.findPersonButton.Text = "Find a person";
+            this.findPersonButton.UseVisualStyleBackColor = true;
+            this.findPersonButton.Click += new System.EventHandler(this.findPersonButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.findPersonButton);
+            this.Controls.Add(this.findPersonTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox findPersonTextBox;
+        private System.Windows.Forms.Button findPersonButton;
+    }
+}
+
---
+++ Visual/FilterRecordProject/FilterRecordProject/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace FilterRecordProject
+{
+    public partial class Form1 : Form
+    {
+        private DataSet _dataset;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadData();
+        }
+
+        private void ReadData()
+        {
+            OleDbConnection connection = CreateConnection();
+
+            OleDbCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Peoples";
+
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            _dataset = new DataSet();
+            adapter.Fill(_dataset);
+            connection.Close();
+        }
+
+        private OleDbConnection CreateConnection()
+        {
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;
+                Persist Security Info=False;Initial Catalog=TestDatabase;
+                Data Source=localhost\SQLEXPRESS";
+
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong while connecting to DB");
+            }
+
+            return connection;
+        }
+
+        private void findPersonButton_Click(object sender, EventArgs e)
+        {
+            string filter = "Фамилия LIKE '" + findPersonTextBox.Text + "%'";
+            DataRow[] rows = _dataset.Tables[0].Select(filter);
+
+            string found = "";
+            foreach (DataRow row in rows)
+                found += row.ItemArray[1].ToString() + "\n";
+
+            MessageBox.Show(found);
+        }
+    }
+}
