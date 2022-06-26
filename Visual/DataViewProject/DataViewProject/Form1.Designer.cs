13e567ae9a4a30e667c4601597a0b8c659ff3fd4
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-26 17:34:02 +0600

    Learning how to use some methods from DataView

---
+++ Visual/DataViewProject/DataViewProject/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DataViewProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.editingButton = new System.Windows.Forms.Button();
+            this.editingButton1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(775, 202);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(13, 260);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(775, 178);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // editingButton
+            // 
+            this.editingButton.Location = new System.Drawing.Point(636, 222);
+            this.editingButton.Name = "editingButton";
+            this.editingButton.Size = new System.Drawing.Size(151, 23);
+            this.editingButton.TabIndex = 3;
+            this.editingButton.Text = "Edit a row idKey2";
+            this.editingButton.UseVisualStyleBackColor = true;
+            this.editingButton.Click += new System.EventHandler(this.editingButton_Click);
+            // 
+            // editingButton1
+            // 
+            this.editingButton1.Location = new System.Drawing.Point(452, 222);
+            this.editingButton1.Name = "editingButton1";
+            this.editingButton1.Size = new System.Drawing.Size(178, 23);
+            this.editingButton1.TabIndex = 4;
+            this.editingButton1.Text = "Edit a row Алексеев";
+            this.editingButton1.UseVisualStyleBackColor = true;
+            this.editingButton1.Click += new System.EventHandler(this.editingButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.editingButton1);
+            this.Controls.Add(this.editingButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button editingButton;
+        private System.Windows.Forms.Button editingButton1;
+    }
+}
+
---
+++ Visual/DataViewProject/DataViewProject/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace DataViewProject
+{
+    public partial class Form1 : Form
+    {
+        DataView view;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ReadData();
+        }
+
+        private void ReadData()
+        {
+            OleDbConnection connection = CreateConnection();
+            OleDbCommand command = new OleDbCommand("SELECT * FROM Peoples");
+            command.Connection = connection;
+
+            // Getting data to dataset
+            OleDbDataAdapter adapter = new OleDbDataAdapter(command);
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+            connection.Close();
+
+            // Creating a view based on a table
+            view = new DataView(dataset.Tables[0]);
+            dataGridView1.DataSource = view;
+            view.Sort = "Имя";
+
+            view.RowFilter = "Фамилия LIKE 'И%'";
+
+            // An example of used row status "Added"
+            Object[] newRow = { null, "Matveyeva", "Matrena" };
+            dataset.Tables[0].LoadDataRow(newRow, false);
+
+            label1.Text = "After adding a row: " + newRow[1].ToString() + " " + 
+                newRow[2].ToString();
+
+            DataView view2 = new DataView(dataset.Tables[0], "", "", 
+                DataViewRowState.Added);
+            dataGridView2.DataSource = view2;
+
+            
+        }
+
+        private OleDbConnection CreateConnection()
+        {
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;
+                Persist Security Info=False;Initial Catalog=TestDatabase;
+                Data Source=localhost\SQLEXPRESS";
+
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong while connecting to DB");
+            }
+
+            return connection;
+        }
+
+        private void editingButton_Click(object sender, EventArgs e)
+        {
+            // Only using already created view
+
+            view.Sort = "idKey";
+            DataRowView[] editrows = view.FindRows(2);
+
+            foreach (DataRowView editrow in editrows)
+                editrow[1] = "Edited";
+        }
+
+        private void editingButton1_Click(object sender, EventArgs e)
+        {
+            view.Sort = "Фамилия";
+            DataRowView[] editrows = view.FindRows("Иванова");
+
+            foreach (DataRowView editrow in editrows)
+                editrow[1] = "Бывшая Иванова";
+        }
+    }
+}
