b1c5d63ff468d043a5b25201d0a80b5f6fef0abd
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-26 18:54:39 +0600

    Learning how to create and use a library

---
+++ Visual/PersonLibrary/PersonLibraryTest/Form1.cs
@@ -0,0 +1,26 @@
+﻿using PersonLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonLibraryTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void testLibraryButton_Click(object sender, EventArgs e)
+        {
+            Person p = new Person();
+        }
+    }
+}
