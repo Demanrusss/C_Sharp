bfc9092ce00755fe3e152206bbfa66717bc4b155
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-26 17:48:13 +0600

    Learning how to get data schema from DB

---
+++ Visual/Schema/Schema/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Schema
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tablesListBox = new System.Windows.Forms.ListBox();
+            this.columnsListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tablesListBox
+            // 
+            this.tablesListBox.FormattingEnabled = true;
+            this.tablesListBox.ItemHeight = 16;
+            this.tablesListBox.Location = new System.Drawing.Point(13, 13);
+            this.tablesListBox.Name = "tablesListBox";
+            this.tablesListBox.Size = new System.Drawing.Size(120, 84);
+            this.tablesListBox.TabIndex = 0;
+            // 
+            // columnsListBox
+            // 
+            this.columnsListBox.FormattingEnabled = true;
+            this.columnsListBox.ItemHeight = 16;
+            this.columnsListBox.Location = new System.Drawing.Point(140, 13);
+            this.columnsListBox.Name = "columnsListBox";
+            this.columnsListBox.Size = new System.Drawing.Size(120, 84);
+            this.columnsListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.columnsListBox);
+            this.Controls.Add(this.tablesListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox tablesListBox;
+        private System.Windows.Forms.ListBox columnsListBox;
+    }
+}
+
---
+++ Visual/Schema/Schema/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Schema
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ReadData();
+        }
+
+        private void ReadData()
+        {
+            OleDbConnection connection = CreateConnection();
+
+            // Getting tables from DB
+            Object[] dbrestrinct = { "TestDatabase", null, null, null};
+            DataTable table = 
+                connection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, 
+                    dbrestrinct);
+
+            foreach (DataRow row in table.Rows)
+                tablesListBox.Items.Add(row["TABLE_NAME"]);
+
+            // Getting schema of columns
+            Object[] clmnrestrinct = { "TestDatabase", null, "Peoples", null };
+            DataTable columns = connection.GetOleDbSchemaTable(OleDbSchemaGuid.Columns,
+                clmnrestrinct);
+
+            foreach (DataRow row in columns.Rows)
+                columnsListBox.Items.Add(row["COLUMN_NAME"]);
+        }
+
+        private OleDbConnection CreateConnection()
+        {
+            OleDbConnection connection = new OleDbConnection();
+            connection.ConnectionString = @"Provider=SQLOLEDB.1;Integrated Security=SSPI;
+                Persist Security Info=False;Initial Catalog=TestDatabase;
+                Data Source=localhost\SQLEXPRESS";
+
+            try
+            {
+                connection.Open();
+            }
+            catch
+            {
+                MessageBox.Show("Something went wrong while connecting to DB");
+            }
+
+            return connection;
+        }
+    }
+}
