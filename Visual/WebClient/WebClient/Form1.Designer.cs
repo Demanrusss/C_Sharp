a3355578c8defc5d633c516a4944c16c92d74568
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-27 20:52:07 +0600

    Load code also with Proxy-server

---
+++ Visual/WebClient/WebClient/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WebClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.loadUrlButton = new System.Windows.Forms.Button();
+            this.useProxyCheckBox = new System.Windows.Forms.CheckBox();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.portTextBox = new System.Windows.Forms.TextBox();
+            this.portLabel = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.userLabel = new System.Windows.Forms.Label();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.passwordLabel = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(14, 8);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(34, 16);
+            this.urlLabel.TabIndex = 0;
+            this.urlLabel.Text = "URL";
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(54, 6);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(835, 22);
+            this.urlTextBox.TabIndex = 1;
+            // 
+            // loadUrlButton
+            // 
+            this.loadUrlButton.Location = new System.Drawing.Point(895, 5);
+            this.loadUrlButton.Name = "loadUrlButton";
+            this.loadUrlButton.Size = new System.Drawing.Size(75, 23);
+            this.loadUrlButton.TabIndex = 2;
+            this.loadUrlButton.Text = "Load";
+            this.loadUrlButton.UseVisualStyleBackColor = true;
+            this.loadUrlButton.Click += new System.EventHandler(this.loadUrlButton_Click);
+            // 
+            // useProxyCheckBox
+            // 
+            this.useProxyCheckBox.AutoSize = true;
+            this.useProxyCheckBox.Location = new System.Drawing.Point(17, 34);
+            this.useProxyCheckBox.Name = "useProxyCheckBox";
+            this.useProxyCheckBox.Size = new System.Drawing.Size(90, 20);
+            this.useProxyCheckBox.TabIndex = 3;
+            this.useProxyCheckBox.Text = "Use proxy";
+            this.useProxyCheckBox.UseVisualStyleBackColor = true;
+            this.useProxyCheckBox.CheckedChanged += new System.EventHandler(this.useProxyCheckBox_CheckedChanged);
+            this.useProxyCheckBox.CheckStateChanged += new System.EventHandler(this.useProxyCheckBox_CheckStateChanged);
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(155, 58);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(58, 16);
+            this.addressLabel.TabIndex = 4;
+            this.addressLabel.Text = "Address";
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.addressTextBox.Enabled = false;
+            this.addressTextBox.Location = new System.Drawing.Point(219, 55);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(152, 22);
+            this.addressTextBox.TabIndex = 5;
+            // 
+            // portTextBox
+            // 
+            this.portTextBox.Enabled = false;
+            this.portTextBox.Location = new System.Drawing.Point(421, 55);
+            this.portTextBox.Name = "portTextBox";
+            this.portTextBox.Size = new System.Drawing.Size(108, 22);
+            this.portTextBox.TabIndex = 7;
+            // 
+            // portLabel
+            // 
+            this.portLabel.AutoSize = true;
+            this.portLabel.Location = new System.Drawing.Point(381, 58);
+            this.portLabel.Name = "portLabel";
+            this.portLabel.Size = new System.Drawing.Size(31, 16);
+            this.portLabel.TabIndex = 6;
+            this.portLabel.Text = "Port";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Enabled = false;
+            this.userTextBox.Location = new System.Drawing.Point(581, 55);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(152, 22);
+            this.userTextBox.TabIndex = 9;
+            // 
+            // userLabel
+            // 
+            this.userLabel.AutoSize = true;
+            this.userLabel.Location = new System.Drawing.Point(537, 58);
+            this.userLabel.Name = "userLabel";
+            this.userLabel.Size = new System.Drawing.Size(36, 16);
+            this.userLabel.TabIndex = 8;
+            this.userLabel.Text = "User";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Enabled = false;
+            this.passwordTextBox.Location = new System.Drawing.Point(818, 55);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(152, 22);
+            this.passwordTextBox.TabIndex = 11;
+            // 
+            // passwordLabel
+            // 
+            this.passwordLabel.AutoSize = true;
+            this.passwordLabel.Location = new System.Drawing.Point(741, 58);
+            this.passwordLabel.Name = "passwordLabel";
+            this.passwordLabel.Size = new System.Drawing.Size(67, 16);
+            this.passwordLabel.TabIndex = 10;
+            this.passwordLabel.Text = "Password";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 91);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(958, 350);
+            this.richTextBox1.TabIndex = 12;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 453);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.passwordLabel);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.userLabel);
+            this.Controls.Add(this.portTextBox);
+            this.Controls.Add(this.portLabel);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.addressLabel);
+            this.Controls.Add(this.useProxyCheckBox);
+            this.Controls.Add(this.loadUrlButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.urlLabel);
+            this.Name = "Form1";
+            this.Text = "Web Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button loadUrlButton;
+        private System.Windows.Forms.CheckBox useProxyCheckBox;
+        private System.Windows.Forms.Label addressLabel;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox portTextBox;
+        private System.Windows.Forms.Label portLabel;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Label userLabel;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label passwordLabel;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Visual/WebClient/WebClient/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace WebClient
+{
+    public partial class Form1 : Form
+    {
+        WebRequest request;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadUrlButton_Click(object sender, EventArgs e)
+        {
+            if (useProxyCheckBox.Checked)
+            {
+                try
+                {
+                    WebProxy proxy = new WebProxy(addressTextBox.Text, Int32.Parse(portTextBox.Text));
+                    proxy.Credentials = new NetworkCredential(userTextBox?.Text, passwordTextBox?.Text);
+
+                    request.Proxy = proxy;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Could not load a file. Maybe, because of proxy\n{ex}");
+                    return;
+                }
+            }
+
+            try
+            {
+                request = HttpWebRequest.Create(urlTextBox.Text);
+                
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                StringBuilder pagebuilder = new StringBuilder();
+
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                    pagebuilder.AppendLine(line);
+
+                response.Close();
+                reader.Close();
+                richTextBox1.Text = pagebuilder.ToString();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Could not load a file");
+                return;
+            }
+        }
+
+        private void useProxyCheckBox_CheckStateChanged(object sender, EventArgs e)
+        {
+            if (useProxyCheckBox.Checked)
+            {
+                addressTextBox.Enabled = true;
+                portTextBox.Enabled = true;
+                userTextBox.Enabled = true;
+                passwordTextBox.Enabled = true;
+            }
+        }
+
+        private void useProxyCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            addressTextBox.Enabled = false;
+            portTextBox.Enabled = false;
+            userTextBox.Enabled = false;
+            passwordTextBox.Enabled = false;
+        }
+    }
+}
