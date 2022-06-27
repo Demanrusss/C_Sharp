5547790c708ee806f34aa1af2e9b2a582366e69e
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-27 15:00:29 +0600

    Creating Remote Server

---
+++ Visual/RemotingServer/RemotingServer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Http;
+using GeneralLibraryPrj;
+
+namespace RemotingServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Application is launched");
+
+            HttpChannel channel = new HttpChannel(32121);
+            ChannelServices.RegisterChannel(channel, false);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(GeneralRemoteClass),
+                "OurFirstSoapProject.soap",
+                WellKnownObjectMode.Singleton);
+            Console.ReadLine();         
+        }
+    }
+}
