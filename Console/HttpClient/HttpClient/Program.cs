983cf4e2690347b1edba18da4febe079ffd6189d
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-27 19:25:00 +0600

    Learning how to use HTTP-client

---
+++ Console/HttpClient/HttpClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace HttpClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebRequest request;
+            string url = @"http://www.flenov.info";
+
+            try
+            {
+                request = HttpWebRequest.Create(url);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not download a file");
+                return;
+            }
+
+            // Another way
+            HttpWebRequest request2;
+            request2 = (HttpWebRequest)HttpWebRequest.Create(url);
+
+            // Reading data from Web-request
+            HttpWebResponse response = (HttpWebResponse)request2.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            StringBuilder pagebuilder = new StringBuilder();
+
+            string line;
+            while ((line = reader.ReadLine()) != null)
+                pagebuilder.AppendLine(line);
+
+            response.Close();
+            reader.Close();
+            Console.WriteLine(pagebuilder.ToString());
+        }
+    }
+}
