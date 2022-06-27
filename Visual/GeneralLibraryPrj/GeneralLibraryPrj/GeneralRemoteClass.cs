8504cad3f5e09e5f9882de8d483be747d049d6d5
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-27 14:28:50 +0600

    Creating a common (Client-Server) build

---
+++ Visual/GeneralLibraryPrj/GeneralLibraryPrj/GeneralRemoteClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GeneralLibraryPrj
+{
+    public class GeneralRemoteClass : MarshalByRefObject
+    {
+        public GeneralRemoteClass()
+        {
+
+        }
+
+        public void SendToSraver(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string ReplyFromSraver()
+        {
+            return "this message";
+        }
+    }
+}
