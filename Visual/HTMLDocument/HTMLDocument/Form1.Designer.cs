a8e9c7c562a36dba3f24521aedb692658a56fec6
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-28 14:08:23 +0600

    Loading HTML-document into our Web_browser

---
+++ Visual/HTMLDocument/HTMLDocument/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace HTMLDocument
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.urlsListBox = new System.Windows.Forms.ListBox();
+            this.browserPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(14, 16);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(34, 16);
+            this.urlLabel.TabIndex = 0;
+            this.urlLabel.Text = "URL";
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(64, 13);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(889, 22);
+            this.urlTextBox.TabIndex = 1;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(959, 13);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 2;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // urlsListBox
+            // 
+            this.urlsListBox.FormattingEnabled = true;
+            this.urlsListBox.ItemHeight = 16;
+            this.urlsListBox.Location = new System.Drawing.Point(12, 41);
+            this.urlsListBox.Name = "urlsListBox";
+            this.urlsListBox.Size = new System.Drawing.Size(1022, 116);
+            this.urlsListBox.TabIndex = 3;
+            // 
+            // browserPanel
+            // 
+            this.browserPanel.Location = new System.Drawing.Point(12, 164);
+            this.browserPanel.Name = "browserPanel";
+            this.browserPanel.Size = new System.Drawing.Size(1022, 386);
+            this.browserPanel.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1046, 562);
+            this.Controls.Add(this.browserPanel);
+            this.Controls.Add(this.urlsListBox);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.urlLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.ListBox urlsListBox;
+        private System.Windows.Forms.Panel browserPanel;
+    }
+}
+
---
+++ Visual/HTMLDocument/HTMLDocument/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTMLDocument
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            urlsListBox.Items.Clear();
+
+            // Loading web-page to our browser
+            WebBrowser browser = new WebBrowser();
+            browser.Navigate(urlTextBox.Text);
+
+            while (browser.ReadyState != WebBrowserReadyState.Complete)
+                Application.DoEvents();
+
+            // Getting all tags <a> and enumerate it
+            HtmlElementCollection elementsByTagName =
+                browser.Document.GetElementsByTagName("a");
+
+            foreach (HtmlElement element in elementsByTagName)
+                urlsListBox.Items.Add(element.GetAttribute("href"));
+
+            foreach (Control c in browserPanel.Controls)
+                c.Dispose();
+
+            browserPanel.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+    }
+}
