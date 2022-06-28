cec91e1456cff877f2a51f1fa2d74fcc0773a2c0
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-06-28 09:30:30 +0600

    Creating UriTest for dividing url-address into segments

---
+++ Visual/UriTest/UriTest/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UriTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            Uri uri = new Uri(urlTextBox.Text);
+
+            listBox1.Items.Add("Host: " + uri.Host.ToString());
+            listBox1.Items.Add("AbsolutePath: " + uri.AbsolutePath.ToString());
+
+            string query = uri.Query.Substring(1, uri.Query.Length - 1);
+            string[] parameters = query.Split('&');
+
+            foreach (string segment in parameters)
+            {
+                string[] paramvalues = segment.Split('=');
+                listBox1.Items.Add("Param: " + paramvalues[0]);
+
+                if (paramvalues.Length > 1)
+                    listBox1.Items.Add("Value: " + paramvalues[1]);
+            }
+        }
+    }
+}
