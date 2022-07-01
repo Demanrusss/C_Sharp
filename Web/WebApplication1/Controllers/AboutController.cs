41d341953a986285e76b013800a0b2b3e5f447ad
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-07-01 20:44:03 +0600

    Hello .Net Core 5.0

---
+++ Web/WebApplication1/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("wof/about")]
+
+    public class AboutController : Controller
+    {
+        [Route("Error")] // It is not index anymore as default
+
+        public string Index(string id, string p)
+        {
+            return "So now 'About' is working too. id=" + id + " p=" + p;
+        }
+    }
+}
---
+++ Web/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello again. Home is working";
+        }
+
+        public string Show()
+        {
+            return "Show is working";
+        }
+    }
+}
