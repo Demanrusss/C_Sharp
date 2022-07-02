5705202961fff0c1ed281505d4ace3a0f0b48195
Demanrusss <102857752+Demanrusss@users.noreply.github.com>
2022-07-02 12:44:36 +0600

    Learning how to use C# and HTML in a View

--- Web/WebApplication1/Controllers/HomeController.cs
+++ Web/WebApplication1/Controllers/HomeController.cs
@@ -8,11 +8,24 @@ namespace WebApplication1.Controllers
         {
             public string FirstName { get; set; }
             public string LastName { get; set; }
+            public int Age { get; set; }
+        }
+
+        public IActionResult List()
+        {
+            Person[] people = new Person[]
+            {
+                new Person { FirstName = "Ivan", LastName = "Ivanov", Age = 10},
+                new Person { FirstName = "Petr", LastName = "Petrov", Age = 20},
+                new Person { FirstName = "Michail", LastName = "Flenov", Age = 45}
+            };
+
+            return View(people);
         }
 
         public IActionResult Index()
         {
-            return View("Index", new Person() { FirstName = "John", LastName = "Doe"});
+            return View("Index", new Person() { FirstName = "John", LastName = "Doe", Age = 10});
         }
 
         public IActionResult Show(int id)
